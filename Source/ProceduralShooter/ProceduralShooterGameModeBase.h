bf18553be35a681f059c0fe0d6bb4ab830880b88
xenono <aurbanczyk0@gmail.com>
2022-02-09 12:55:55 +0000

    Project Initialization

---
+++ Source/ProceduralShooter/ProceduralShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALSHOOTER_API AProceduralShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
